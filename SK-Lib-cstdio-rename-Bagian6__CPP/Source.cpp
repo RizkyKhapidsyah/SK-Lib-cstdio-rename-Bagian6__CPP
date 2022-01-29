0dc80d6b732dcd0e59304a8bb75f3ecbdcc06fb2
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2022-01-29 12:10:57 +0700

    Add project files.

---
+++ SK-Lib-cstdio-rename-Bagian6__CPP/Source.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+#include <conio.h>
+
+/*
+    Source by CPPReference
+    Modified For Learn by RK
+    I.D.E : VS2019
+*/
+
+namespace fs = std::filesystem;
+
+int main() {
+    fs::path p = fs::current_path() / "sandbox";
+    fs::create_directories(p / "from");
+    std::ofstream(p / "from/file1.txt").put('a');
+    fs::create_directory(p / "to");
+
+    //    fs::rename(p/"from/file1.txt", p/"to/"); // error: to is a directory
+    fs::rename(p / "from/file1.txt", p / "to/file2.txt"); // OK
+//    fs::rename(p/"from", p/"to"); // error: to is not empty
+    fs::rename(p / "from", p / "to/subdir"); // OK
+    fs::remove_all(p);
+
+    _getch();
+    retutrn 0;
+}
